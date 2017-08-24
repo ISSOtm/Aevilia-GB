5e2f3519b5a59a9e042b7cbf59088650b96bb54d
ISSOtm <eldredhabert0@gmail.com>
2017-08-24 23:03:17 +0200

    Make Windows compiler script create bin and obj dirs

--- compiler.bat
+++ compiler.bat
@@ -1,6 +1,10 @@
 @echo off
 set ROMVERSION=0
 title Compiling AEVILIA GB version 1.%ROMVERSION%
+
+if NOT exist bin/	mkdir bin
+if NOT exist obj/	mkdir obj
+
 echo === COMPILATION ===
 rgbasm -E -p 0xFF -o obj/main.o			 main.asm
 rgbasm -E -p 0xFF -o obj/battle.o		 battle.asm
@@ -15,17 +19,20 @@ echo.
 echo COMPILATION DONE
 echo.
 echo.
+
 echo === LINKING ===
 rgblink -m bin/Aevilia.map -n bin/Aevilia.sym -o bin/aevilia.gbc obj/main.o obj/battle.o obj/engine.o obj/home.o obj/gfx.o obj/maps.o obj/save.o obj/sound.o obj/tileset.o
 echo.
 echo LINKING DONE
 echo.
 echo.
+
 echo === FIXING ===
 rgbfix -Cjv -i ISSO -k 42 -l 0x33 -m 0x1B -n %ROMVERSION% -p 0xFF -r 0x04 -t AEVILIA bin/aevilia.gbc
 echo.
 echo FIXING DONE
 echo.
 echo.
+
 echo FILES READY !
 pause
