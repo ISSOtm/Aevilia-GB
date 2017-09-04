196a58b0d4e75e97eda70a950175f0900953ace7
kaikun97 <animedude008@gmail.com>
2017-09-04 13:54:25 +0100

    compiler changes, new versioning system. RGBFIX now relies on build number instead of version

--- compiler.bat
+++ compiler.bat
@@ -1,11 +1,12 @@
 @echo off
-set ROMVERSION=0
-title Compiling AEVILIA GB version 1.%ROMVERSION%
+set /p VER=<version.txt
+set /p BUILD=<build.txt
+title AEVILIA GB version %VERSION% build %BUILD%
 
 if NOT exist bin/	mkdir bin
 if NOT exist obj/	mkdir obj
 
-echo === COMPILATION ===
+echo === Compiling... ===
 rgbasm -E -p 0xFF -o obj/main.o			 main.asm
 rgbasm -E -p 0xFF -o obj/battle.o		 battle.asm
 rgbasm -E -p 0xFF -o obj/engine.o		 engine.asm
@@ -17,23 +18,23 @@ rgbasm -E -p 0xFF -o obj/sound.o		 sound.asm
 rgbasm -E -p 0xFF -o obj/text.o          text.asm
 rgbasm -E -p 0xFF -o obj/tileset.o		 tileset.asm
 echo.
-echo COMPILATION DONE
+echo Compile finished.
 echo.
 echo.
 
-echo === LINKING ===
+echo === Linking... ===
 rgblink -m bin/Aevilia.map -n bin/Aevilia.sym -o bin/aevilia.gbc obj/main.o obj/battle.o obj/engine.o obj/home.o obj/gfx.o obj/maps.o obj/save.o obj/sound.o obj/text.o obj/tileset.o
 echo.
-echo LINKING DONE
+echo Linking finished.
 echo.
 echo.
 
-echo === FIXING ===
-rgbfix -Cjv -i ISSO -k 42 -l 0x33 -m 0x1B -n %ROMVERSION% -p 0xFF -r 0x04 -t AEVILIA bin/aevilia.gbc
+echo === Fixing... ===
+rgbfix -Cjv -i ISSO -k 42 -l 0x33 -m 0x1B -n %BUILD% -p 0xFF -r 0x04 -t AEVILIA bin/aevilia.gbc
 echo.
-echo FIXING DONE
+echo Fixing finished.
 echo.
 echo.
 
-echo FILES READY !
+echo Build successful.
 pause
