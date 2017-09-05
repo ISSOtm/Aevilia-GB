43c2b49ab088c40c77e646e2d41d015444bb8bb6
ISSOtm <eldredhabert0@gmail.com>
2017-09-05 16:51:46 +0200

    Woops forgot player house strings

--- text.asm
+++ text.asm
@@ -3,3 +3,4 @@ INCLUDE "macros.asm"
 INCLUDE "constants.asm"
 
 INCLUDE "text/startham.asm"
+INCLUDE "text/playerhouse.asm"
--- text/playerhouse.asm
+++ text/playerhouse.asm
@@ -2,4 +2,7 @@
 SECTION "Player house text strings", ROMX
 	
 	set_text_prefix PlayerHouseTV
-	dline 0, "Wanna watch TV?"
+	dline 0, "Wanna watch TV for"
+	dline 1, "for a bit?"
+	
+	dline 2, "Yeah\, let's not."
