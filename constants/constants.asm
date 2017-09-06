8a8843bc0c2db7ffe52132e07317f3f808f186d8
ISSOtm <eldredhabert0@gmail.com>
2017-09-06 02:30:04 +0200

    Added constant forgotten in 891fc1f

--- constants/constants.asm
+++ constants/constants.asm
@@ -45,6 +45,8 @@ VRAM_ROW_SIZE	EQU $20 ; Size of a row of tiles in VRAM
 
 TITLE_NAME_DEST	EQU $9865
 
+TEXTBOX_MOVEMENT_SPEED	EQU 4
+
 ; -------------- MAP ----------------
 NB_OF_NPCS		EQU 8
 NO_SCRIPT		EQU $0000
