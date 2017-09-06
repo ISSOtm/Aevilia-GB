ISSOtm/Aevilia-GB

commit cbd8fe018e7669117a24852bb2fd5894e78c4a9d

Prevent buffer overflow when loading tilesets

home/map.asm
@@ -552,7 +552,7 @@ LoadTileset::
 	pop hl
 .noAnimators
 	
-	ld de, wBGPalette7_color0
+	ld de, wOBJPalette6_color2
 	ld c, 14
 	rst copy
 	push hl
@@ -577,7 +577,7 @@ LoadTileset::
 	
 	restore_rom_bank
 	pop hl
-	ld de, wOBJPalette7_color0
+	ld de, wOBJPalette6_color2
 	ld c, 14
 	rst copy
 ;	push hl

commit 62a9063041659b6abcf8c23e0a70ca1fbe5ad489

Fix tileset loading (broken by cbd8fe0)

home/map.asm
@@ -560,7 +560,7 @@ LoadTileset::
 	ld a, BANK(DefaultPalette)
 	rst bankswitch
 	
-	ld hl, wBGPalette7_color0
+	ld hl, wOBJPalette6_color2
 	ld de, wBGPalette1_color0
 .loadTilesetBGPalettes
 	ld a, [hli]
@@ -585,7 +585,7 @@ LoadTileset::
 	ld a, BANK(DefaultPalette)
 	rst bankswitch
 	
-	ld hl, wOBJPalette7_color0
+	ld hl, wOBJPalette6_color2
 	ld de, wOBJPalette1_color0
 .loadTilesetOBJPalettes
 	inc de