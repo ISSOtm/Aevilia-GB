7bfd982017a5b8b0d2e5c685196910321fd883c9
ISSOtm <eldredhabert0@gmail.com>
2017-10-05 02:02:03 +0200

    Add a few flags

--- constants/flags.asm
+++ constants/flags.asm
@@ -1,5 +1,37 @@
 
+; Constants for the interactions flag dependencies
+flag_dep: MACRO
+	dw \1 | \2
+ENDM
+FLAG_RESET	equ $0000
+FLAG_SET	equ $8000
+
+FLAG_DEP	equ $80
+	
+	
+	; Flag IDs
+	; Please group by 8 to help locate them in memory !
 	enum_start
 	
-	enum_elem	FLAG_
+	enum_elem	FLAG_INTRO_MAP_STATUS_0
+	enum_elem	FLAG_INTRO_MAP_STATUS_1
+	enum_elem	FLAG_INTRO_MAP_STATUS_2
+	enum_elem	FLAG_INTRO_MAP_STATUS_3
+	enum_elem	FLAG_INTRO_MAP_UNUSED
+	enum_elem	FLAG_INTRO_MAP_UNUSED2
+	enum_elem	FLAG_INTRO_MAP_256_FRAMES
+	enum_elem	FLAG_INTRO_MAP_SKIPPED_TUTORIAL
+	
+	enum_elem	FLAG_INTRO_MAP_DELAY_0
+	enum_elem	FLAG_INTRO_MAP_DELAY_1
+	enum_elem	FLAG_INTRO_MAP_DELAY_2
+	enum_elem	FLAG_INTRO_MAP_DELAY_3
+	enum_elem	FLAG_INTRO_MAP_DELAY_4
+	enum_elem	FLAG_INTRO_MAP_DELAY_5
+	enum_elem	FLAG_INTRO_MAP_DELAY_6
+	enum_elem	FLAG_INTRO_MAP_DELAY_7
+	
+	enum_elem	FLAG_TEST_WARRIOR_SPOKE_ONCE
+	enum_elem	FLAG_STARTHAM_LARGE_HOUSE_UNLOCKED
+	enum_elem	FLAG_STARTHAM_SIBLING_ENTERED
 	
