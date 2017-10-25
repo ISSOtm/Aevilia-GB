8145e365bb263d0e0fb9be2141434b0f9100b978
ISSOtm <eldredhabert0@gmail.com>
2017-10-25 03:21:48 +0200

    Add forgotten text files

--- text/playerhouse.asm
+++ text/playerhouse.asm
@@ -10,15 +10,54 @@ SECTION "Player house text strings", ROMX
 	dline 2, "Yeah\, let's not."
 	
 	dline 3, "Hey!"
-	dline 4, "I'm watching"
-	dline 5, "the TV\, there!"
+	dline 4, "I'm watching the"
+	dline 5, "TV\, there!"
 	dline 6, "You're not"
-	dline 7, "transparent\,"
-	dline 8, "you know!"
+	dline 7, "transparent\, you"
+	dline 8, "know\, ",2,"!"
 	
 	
 	set_text_prefix PlayerHouseSiblingTVScript
 	dname 1
-	dline 0, "I'm watching"
-	dline 1, "TV\, so please"
-	dline 2, "don't disturb."
+	dline 0, "I know I'm"
+	dline 1, "bothering you\,"
+	dline 2, "but I'd like to"
+	dline 3, "stay a bit before"
+	dline 4, "leaving."
+	dline 5, "I'm sorry."
+	
+	
+	set_text_prefix PlayerHouseDontLeaveScript
+	dname 1
+	dline 0, "Wait!"
+	dline 1, "Um\, ",2,"..."
+	dline 2, "I know I didn't"
+	dline 3, "tell you before"
+	dline 4, "coming\, but..."
+	dline 5, "Can I spend some"
+	dline 6, "time with you?"
+	
+	dline 7, "Really? Oh\, ",2,"\,"
+	dline 8, "you're the best!"
+	; 14
+	; 15
+	dline 19, "I really missed"
+	dline 20, "seeing you\, ",2,"."
+	
+	dline 9, "..."
+	dline 10, "Look\, ",2,"\, I know"
+	dline 11, "that was rude\, but"
+	dline 12, "would you really"
+	dline 13, "turn me away?"
+	dline 14, "It's been almost a"
+	dline 15, "year\, now."
+	dline 16, "It's alright\,"
+	dline 17, "then?"
+	dline 18, "Thank you."
+	
+	dline 21, 1," joined you!"
+	
+	
+OkayNoChoice::
+	dstr "Okay"
+	dstr "No"
