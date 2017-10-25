f95195edc61fc31e2cd70cfd1fafd5f3926e7f1f
ISSOtm <eldredhabert0@gmail.com>
2017-10-25 03:22:21 +0200

    Add forgotten flag ID

--- constants/flags.asm
+++ constants/flags.asm
@@ -17,12 +17,12 @@ FLAG_DEP	equ $80
 	enum_elem	FLAG_INTRO_MAP_STATUS_1
 	enum_elem	FLAG_INTRO_MAP_STATUS_2
 	enum_elem	FLAG_INTRO_MAP_STATUS_3
+	enum_elem	FLAG_INTRO_CUTSCENE_PLAYED ; Set when the cutscene in the player's bed played
 	enum_elem	FLAG_INTRO_MAP_UNUSED
-	enum_elem	FLAG_INTRO_MAP_UNUSED2
 	enum_elem	FLAG_INTRO_MAP_256_FRAMES
 	enum_elem	FLAG_INTRO_MAP_SKIPPED_TUTORIAL
 	
-	enum_elem	FLAG_INTRO_MAP_DELAY_0
+	enum_elem	FLAG_INTRO_MAP_DELAY_0 ; These are accessed as a byte (directly) only
 	enum_elem	FLAG_INTRO_MAP_DELAY_1
 	enum_elem	FLAG_INTRO_MAP_DELAY_2
 	enum_elem	FLAG_INTRO_MAP_DELAY_3
@@ -34,6 +34,6 @@ FLAG_DEP	equ $80
 	enum_elem	FLAG_TEST_WARRIOR_SPOKE_ONCE
 	enum_elem	FLAG_STARTHAM_LARGE_HOUSE_UNLOCKED
 	enum_elem	FLAG_STARTHAM_SIBLING_ENTERED
-	enum_elem	FLAG_LOAD_CUTSCENE_NPCS ; Use with NPCs that should only be loaded for a cutscene (reset after loading)
+	enum_elem	FLAG_LOAD_CUTSCENE_NPCS ; Use with NPCs that should only be loaded for a cutscene (make sure to reset after loading)
 	enum_elem	FLAG_SIBLING_WATCHING_TV
 	
