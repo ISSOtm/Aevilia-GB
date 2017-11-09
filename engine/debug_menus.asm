dca31fec10293f126db723193d384073bb93dc00
ISSOtm <eldredhabert0@gmail.com>
2017-11-09 20:15:24 +0100

    Add NPC viewer menu

--- engine/debug_menus.asm
+++ engine/debug_menus.asm
@@ -17,6 +17,13 @@ HomeDebugMenu::
 	ld hl, wFixedTileMap
 	ld bc, SCREEN_HEIGHT * SCREEN_WIDTH
 	call Fill
+	inc a ; ld a, 1
+	ld [rVBK], a
+	ld hl, vFixedMap
+	ld bc, SCREEN_HEIGHT * VRAM_ROW_SIZE
+	xor a
+	call FillVRAM
+	ld [rVBK], a
 	
 	ld hl, wTransferRows + 8
 	inc a ; ld a, 1
@@ -107,15 +114,7 @@ HomeDebugMenu::
 	
 SoundTestMenu::
 	ld hl, .strings
-.printStrings
-	ld e, [hl]
-	inc hl
-	ld d, [hl]
-	inc hl
-	rst copyStr
-	ld a, [hl]
-	and a
-	jr nz, .printStrings
+	call DebugMenuPrintStrings
 	
 	ld hl, wTransferRows + 8
 	ld c, SCREEN_HEIGHT
@@ -288,7 +287,10 @@ TilesetViewerMenu::
 	ld [wLoadedTileset], a
 	
 .restartNoReset
-	ld a, 1
+	xor a
+	ld [wNumOfSprites], a
+	inc a ; ld a, 1
+	ld [wTransferSprites], a
 	ld [rVBK], a
 	ld hl, vFixedMap
 	ld b, SCREEN_HEIGHT
@@ -395,6 +397,7 @@ TilesetViewerMenu::
 	jr .selectTileset
 	
 .left
+	ld hl, .mainFunctions
 	call .loadAction
 	ld a, [de]
 	and a
@@ -402,6 +405,7 @@ TilesetViewerMenu::
 	dec a
 	jr .performAction
 .right
+	ld hl, .mainFunctions
 	call .loadAction
 	ld a, [de]
 	inc a
@@ -520,10 +524,135 @@ TilesetViewerMenu::
 	ld [rVBK], a
 	ret
 	
+	
 .viewNPCs
+	ld hl, wFixedTileMap + SCREEN_WIDTH * 3
+	ld bc, SCREEN_WIDTH * (SCREEN_HEIGHT - 3)
+	xor a
+	call Fill
+	ld hl, .NPCStrings
+	call DebugMenuPrintStrings
+	ld hl, wTransferRows + 8
+	ld c, SCREEN_HEIGHT
+	inc a ; ld a, 1
+	rst fill
+	
+	ld [rVBK], a
+	ld hl, $9E4C
+	ld de, VRAM_ROW_SIZE
+	ld b, 6
+.writeFlipBits
+	rst isVRAMOpen
+	jr nz, .writeFlipBits
+	ld [hl], $20
+	add hl, de
+	dec b
+	jr nz, .writeFlipBits
+	xor a
+	ld [rVBK], a
+	
+	ld a, [wYPos]
+	ld [wXPos + 1], a
+	
+	ld hl, wNPC1_ypos
+	ld c, 4
+	xor a
+	rst fill
+	ld [wYPos], a
+	ld [wLoadedMap], a
+	
+	ld [wNumOfNPCs], a
+	ld [wNPC0_ypos + 1], a
+	ld [wNPC0_xpos + 1], a
+	ld a, $20
+	ld [wNPC0_ypos], a
+	ld [wNPC0_xpos], a
+	call .loadMapNPCTiles
+	call .updateNPCPalettes
+	
+.NPCMainLoop
+	rst waitVBlank
+	ldh a, [hPressedButtons]
+	bit 1, a
+	jr nz, .restoreAndExit
+	rla
+	jr c, .npcDown
+	rla
+	jr c, .npcUp
+	rla
+	jr c, .npcLeft
+	rla
+	jr nc, .NPCMainLoop
+	
+	; right
+	ld hl, .NPCFunctions
+	call .loadAction
+	ld a, [de]
+	inc a
+	cp c
+	jr z, .NPCMainLoop
+	jr .performNPCAction
+.npcLeft
+	ld hl, .NPCFunctions
+	call .loadAction
+	ld a, [de]
+	and a
+	jr z, .NPCMainLoop
+	dec a
+.performNPCAction
+	ld [de], a
+	ld c, a
+	push hl
+	ld a, [wYPos]
+	call .getCursorPos
+	ld d, h
+	ld e, l
+	inc de
+	ld a, b
+	add a, 18
+	ld l, a
+	ld h, HIGH(wTransferRows)
+	ld [hl], a
+	ld b, c
+	call PrintHex
+	pop hl
+	ld a, [hli]
+	ld h, [hl]
+	ld l, a
+	rst callHL
+	jr .NPCMainLoop
+	
+.npcUp
+	ld a, [wYPos]
+	and a
+	jr z, .NPCMainLoop
+	dec a
+	jr .moveNPCCursor
+.npcDown
+	ld a, [wYPos]
+	inc a
+	cp 6
+	jr z, .NPCMainLoop
+.moveNPCCursor
+	ld c, a
+	ld d, 0
+	ld a, [wYPos]
+	call .writeCursor
+	ld a, c
+	ld [wYPos], a
+	ld d, $7F
+	call .writeCursor
+	jr .NPCMainLoop
+	
+.restoreAndExit
+	ld a, [wXPos + 1]
+	ld [wYPos], a
 	jp TilesetViewerMenu
 	
 	
+	; Utilitary functions
+	
+	; Code for moving the cursor around
 .getCursorPos
 	ld b, a
 	add a, a
@@ -554,16 +683,15 @@ TilesetViewerMenu::
 	rst waitVBlank
 	ret
 	
-	; Code for moving the cursor around
 .loadAction
 	ld a, [wYPos]
 	ld b, a
 	add a, a
 	add a, a
 	add a, b
-	add a, LOW(.functions)
+	add a, l
 	ld l, a
-	adc a, HIGH(.functions)
+	adc a, h
 	sub l
 	ld h, a
 	ld a, [hli]
@@ -574,7 +702,8 @@ TilesetViewerMenu::
 	ld c, a
 	ret
 	
-.functions
+	
+.mainFunctions
 	dw wLoadedTileset
 	db NB_OF_TILESETS
 	dw .loadTileset
@@ -584,14 +713,30 @@ TilesetViewerMenu::
 	dw .updateBlockView
 	
 .loadTileset
+	ld hl, v1Tiles1
+	call .resetTileBlock
+	ld a, [wLoadedTileset]
+	call LoadTileset
+	callacross ReloadPalettes
+	; Fall through because block needs to be redrawn
+	
+.updateBlockView
+	ld a, [wYPos + 1]
+	ld de, $9EAD
+	jp DrawBlock
+	
+	
+.resetTileBlock
+	push hl
+	push hl
 	ld d, $FD
 .clearOneBank
 	inc d
-	jr z, .doneClearingTiles
+	ret z
 	ld a, d
 	cpl
 	ld [rVBK], a
-	ld hl, v1Tiles1
+	pop hl
 	ld bc, 4 * 256
 	ld e, $AA
 .clearTiles
@@ -607,16 +752,171 @@ TilesetViewerMenu::
 	or c
 	jr nz, .clearTiles
 	jr .clearOneBank
-.doneClearingTiles
-	ld a, [wLoadedTileset]
-	call LoadTileset
-	callacross ReloadPalettes
-	; Fall through because block needs to be redrawn
 	
-.updateBlockView
-	ld a, [wYPos + 1]
-	ld de, $9EAD
-	jp DrawBlock
+	
+.NPCFunctions
+	dw wLoadedMap
+	db NB_OF_MAPS
+	dw .loadMapNPCTiles
+	
+	dw wNPC0_sprite
+	db $55
+	dw ProcessNPCs
+	
+	dw wNPC1_ypos
+	db 8
+	dw .updateNPCPalettes
+	
+	dw wNPC1_ypos + 1
+	db 8
+	dw .updateNPCPalettes
+	
+	dw wNPC1_ypos + 2
+	db 8
+	dw .updateNPCPalettes
+	
+	dw wNPC1_ypos + 3
+	db 8
+	dw .updateNPCPalettes
+	
+.loadMapNPCTiles
+	ld hl, v1Tiles0
+	call .resetTileBlock
+	
+	ld b, BANK(MapROMBanks)
+	ld d, b
+	ld h, HIGH(MapROMBanks)
+	ld a, [wLoadedMap]
+	ld e, a
+	ld l, a
+	call GetByteAcross
+	ld c, b ; Bank
+	
+	ld a, e
+	add a, a
+	add a, LOW(MapPointers)
+	ld l, a
+	adc a, HIGH(MapPointers)
+	sub l
+	ld h, a
+	ld b, d
+	call GetByteAcross
+	inc hl
+	ld b, d
+	ld d, a ; Low byte
+	call GetByteAcross
+	
+	; c = bank
+	; d = low
+	; a = high
+	ld b, c
+	ld h, a
+	ld a, d
+	add a, 10 ; Skip over header
+	ld l, a
+	jr nc, .noCarry1
+	inc h
+.noCarry1
+	call GetByteAcross ; Num of interactions
+	and a
+	jr z, .skippedInteractions
+	ld e, b
+	inc hl
+.skipInteractions
+	ld b, c
+	call GetByteAcross
+	rla
+	jr nc, .noDep
+	inc hl ; Skip over dependency
+	inc hl
+.noDep
+	ld a, l
+	add a, INTERACTION_STRUCT_SIZE + 1 ; Skip over type, too
+	ld l, a
+	jr nc, .noCarry2
+	inc h
+.noCarry2
+	dec e ; That was one.
+	jr nz, .skipInteractions
+.skippedInteractions
+	ld b, c
+	call GetByteAcross ; NPC count
+	add a, a
+	ret z ; If no NPCs, then nothing to load
+	add a, a
+	add a, a
+	sub b ; Trick : b was already loaded with the original copy of a !
+	inc a
+	inc a ; Skip nb of NPCs, nb of scripts, ptr (this skips 4 bytes because *2)
+	add a, a
+	add a, l
+	ld l, a
+	jr nc, .noCarry3
+	inc h
+.noCarry3
+	ld b, c
+	call GetByteAcross ; get tile count
+	inc hl
+	and a
+	ret z ; If no tiles...
+	ld de, $80C0
+	ld b, a
+.loadNPCTiles
+	push bc ; Save count and bank
+	ld b, c
+	call GetByteAcross ; Tile bank
+	inc hl
+	and a ; Check if bank is zero (special trap)
+	jr z, .loadSiblingGfx
+	inc hl
+	inc hl
+	push hl ; Save ptr to next entry
+	; Done before pushing de for obvious reasons
+	push de
+	ld e, a ; Store bank in e
+	dec hl
+	ld b, c
+	call GetByteAcross
+	ld d, a ; Store high
+	ld b, c
+	dec hl
+	call GetByteAcross
+	ld l, a
+	ld h, d
+	ld a, e ; bank
+	pop de
+.loadGfx
+	ld bc, $C0
+	call CopyAcrossToVRAM
+	pop hl
+	pop bc
+	dec b
+	jr nz, .loadNPCTiles
+	ret
+.loadSiblingGfx
+	push hl
+	ld a, [wPlayerGender]
+	and a
+	ld hl, EvieTiles
+	ld a, BANK(EvieTiles)
+	jr nz, .loadGfx
+	ld hl, TomTiles
+	jr .loadGfx
+	
+.updateNPCPalettes
+	ld hl, wNPC1_ypos
+	ld de, wNPC0_palettes
+	ld b, 2
+.updatePaletteByte
+	ld a, [hli]
+	swap a
+	or [hl]
+	ld [de], a
+	inc hl
+	inc e ; inc de
+	dec b
+	jr nz, .updatePaletteByte
+	jp ProcessNPCs
 	
 	
 .strings
@@ -633,4 +933,32 @@ TilesetViewerMenu::
 	dw wFixedTileMap + SCREEN_WIDTH * 11
 	dstr "  BLOCK ID:  00 "
 	db 0
+	
+	
+.NPCStrings
+	dw wFixedTileMap + SCREEN_WIDTH * 10
+	dstr "    MAP ID: ",$7F,"00",$7F
+	dw wFixedTileMap + SCREEN_WIDTH * 11
+	dstr "DISPLAY ID:  00 "
+	dw wFixedTileMap + SCREEN_WIDTH * 12
+	dstr "PALETTE TL:  00 "
+	dw wFixedTileMap + SCREEN_WIDTH * 13
+	dstr "PALETTE BL:  00 "
+	dw wFixedTileMap + SCREEN_WIDTH * 14
+	dstr "PALETTE TR:  00 "
+	dw wFixedTileMap + SCREEN_WIDTH * 15
+	dstr "PALETTE BR:  00 "
+	db 0
+	
+	
+DebugMenuPrintStrings::
+	ld e, [hl]
+	inc hl
+	ld d, [hl]
+	inc hl
+	rst copyStr
+	ld a, [hl]
+	and a
+	jr nz, DebugMenuPrintStrings
+	ret
 	
