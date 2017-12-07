3277b338144ac0b6c92189b6e3d276f9e954f606
DevEd2 <deved8@gmail.com>
2017-12-07 12:57:39 -0500

    SFX when exiting building

--- maps/starthamlargehouse1f.asm
+++ maps/starthamlargehouse1f.asm
@@ -15,7 +15,7 @@ StarthamLargeHouseInteractions::
 	db 1
 	
 	db WALK_LOADZONE
-	load_zone $0077, $003E, 10, $15, THREAD2_LOADINGWALKDOWN, 3, MAP_STARTHAM, SFX_NONE
+	load_zone $0077, $003E, 10, $15, THREAD2_LOADINGWALKDOWN, 3, MAP_STARTHAM, SFX_STAIRS
 	
 StarthamLargeHouseNPCs::
 	db 1
