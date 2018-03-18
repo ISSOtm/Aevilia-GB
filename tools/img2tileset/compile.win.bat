4a8181d2689791372a54ef33d5189179d16b65a3
ISSOtm <eldredhabert0@gmail.com>
2018-03-19 00:41:25 +0100

    Remove broken compiler exe
    This reverts commit 2d2b2591a2f999cd848d45a1dc1f6867399c932c.

--- tools/img2tileset/compile.win.bat
+++ tools/img2tileset/compile.win.bat
@@ -4,9 +4,5 @@ copy build\img2tileset\warnimg2tileset.txt warn.log > nul
 del build\img2tileset\*.* /f /q > nul
 rmdir build\img2tileset > nul
 rmdir build > nul
-copy dist\*.* *.* > nul
-del dist\*.* /f /q
-del __pycache__\*.* /f /q
-rmdir __pycache__
+move dist\*.* *.* > nul
 rmdir dist
-del img2tileset.spec
